e3af5b93c1b4d093297856e65e77aaa641037877
sam mok <sam@jzdoctor.com>
2020-03-13 16:19:00 +0800

    Added new.js

---
+++ new.js
@@ -0,0 +1,44 @@
+function Otaku (name, age) {
+  this.name = name;
+  this.age = age;
+
+  this.habit = 'Games';
+}
+
+Otaku.prototype.strength = 60;
+
+Otaku.prototype.sayYourName = function () {
+  console.log('I am ' + this.name);
+}
+
+function objectFactory() {
+  var obj = new Object(),
+  Constructor = [].shift.call(arguments);
+  obj.__proto__ = Constructor.prototype;
+  Constructor.apply(obj, arguments);
+  return obj;
+};
+
+var person = objectFactory(Otaku, 'Kevin', '18')
+
+console.log(person.name) // Kevin
+console.log(person.habit) // Games
+console.log(person.strength) // 60
+
+person.sayYourName(); // I am Kevin
+
+
+//  考虑是否有返回值
+function objectFactory() {
+
+  var obj = new Object(),
+
+  Constructor = [].shift.call(arguments);
+
+  obj.__proto__ = Constructor.prototype;
+
+  var returnValue = Constructor.apply(obj, arguments);
+
+  return typeof returnValue === 'object' ? returnValue : obj;
+
+};
