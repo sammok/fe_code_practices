414e783058fdaaa9a956076854a7fe1f7f6e34c9
sam mok <sam@jzdoctor.com>
2020-03-15 17:16:33 +0800

    Added implemeent symbol.

---
+++ implement_symbol.js
@@ -0,0 +1,73 @@
+// https://github.com/mqyqingfeng/Blog/issues/87
+
+(function() {
+  var root = this;
+
+  var generateName = (function(){
+      var postfix = 0;
+      return function(descString){
+          postfix++;
+          return '@@' + descString + '_' + postfix
+      }
+  })()
+
+  var SymbolPolyfill = function Symbol(description) {
+
+      if (this instanceof SymbolPolyfill) throw new TypeError('Symbol is not a constructor');
+
+      var descString = description === undefined ? undefined : String(description)
+
+      var symbol = Object.create({
+          toString: function() {
+              return this.__Name__;
+          },
+          valueOf: function() {
+              return this;
+          }
+      })
+
+      Object.defineProperties(symbol, {
+          '__Description__': {
+              value: descString,
+              writable: false,
+              enumerable: false,
+              configurable: false
+          },
+          '__Name__': {
+              value: generateName(descString),
+              writable: false,
+              enumerable: false,
+              configurable: false
+          }
+      });
+
+      return symbol;
+  }
+
+  var forMap = {};
+
+  Object.defineProperties(SymbolPolyfill, {
+      'for': {
+          value: function(description) {
+              var descString = description === undefined ? undefined : String(description)
+              return forMap[descString] ? forMap[descString] : forMap[descString] = SymbolPolyfill(descString);
+          },
+          writable: true,
+          enumerable: false,
+          configurable: true
+      },
+      'keyFor': {
+          value: function(symbol) {
+              for (var key in forMap) {
+                  if (forMap[key] === symbol) return key;
+              }
+          },
+          writable: true,
+          enumerable: false,
+          configurable: true
+      }
+  });
+
+  root.SymbolPolyfill = SymbolPolyfill;
+
+})()
